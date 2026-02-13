42261d83a17053eea0b47d3293728cb32dc2dbb5
Rebecca <rirving@ualberta.ca>
2026-02-13 16:20:21 -0700

    Commit 2 from demo

---
+++ app/src/test/java/com/example/listycity/CityListTest.java
@@ -0,0 +1,49 @@
+package com.example.listycity;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CityListTest {
+    private CityList mockCityList() {
+        CityList cityList = new CityList();
+        cityList.add(mockCity());
+        return cityList;
+    }
+    private City mockCity() {
+        return new City("Edmonton", "Alberta");
+    }
+
+    @Test
+    void testAdd() {
+        CityList cityList = mockCityList();
+        assertEquals(1, cityList.getCities().size());// 1 bcs size of get cities is 1
+        City city = new City("Calgary", "Alberta");
+        cityList.add(city);
+        assertEquals(2, cityList.getCities().size());
+        assertTrue(cityList.getCities().contains(city));
+    }
+
+    @Test
+    void testAddException() {
+        CityList cityList = mockCityList();
+        City city = new City("Winnepeg", "Manitoba");
+        cityList.add(city);
+        assertThrows(IllegalArgumentException.class, () -> {
+            cityList.add(city);
+        });
+    }
+
+    @Test
+    void testGetCities() {
+        CityList cityList = mockCityList();
+        // This line checks if the first city in the cityList (retrieved by cityList.getCities().get(0))
+        // is the same as the city returned by mockCity()
+        assertEquals(0, mockCity().compareTo(cityList.getCities().get(0)));
+        // This pushes down the original city
+        City city = new City("Calgary", "Alberta");
+        cityList.add(city);
+        // Now the original city should be at position 1
+        assertEquals(0, city.compareTo(cityList.getCities().get(0)));
+        assertEquals(0, mockCity().compareTo(cityList.getCities().get(1)));
+    }
+}
